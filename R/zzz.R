b075d6e706a4301fb816a87403daf8927763853f
Chan-Yub Park <mrchypark@gmail.com>
2018-09-29 02:27:34 +0900

    Update zzz.R

--- R/zzz.R
+++ R/zzz.R
@@ -1,5 +1,3 @@
 .onAttach <- function(libname, pkgname) {
   packageStartupMessage("krlift 패키지를 설치해주셔서 감사합니다!")
-  packageStartupMessage("비록 한개의 데이터만 있지만 connection pane을 확인해주세요.")
-  fryingpane::cook("krlift")
 }
